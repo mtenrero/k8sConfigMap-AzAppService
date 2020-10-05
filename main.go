ed76e9e88f128d82c7ba526a5e2b95f1e8bb97ed
Marcos Tenrero <mtenreromoran@cegid.com>
2020-10-05 18:20:22 +0200

    Added some verbosing

--- main.go
+++ main.go
@@ -13,15 +13,26 @@ func main() {
 	var outPath = flag.String("out", "./azout.json", "Specify target JSON file path")
 	flag.Parse()
 
+	log.Println("Parsing ConfigMap YAML file...")
+
 	envVars, err := cmapparser.ParseConfigMapEnvVars(*filePath)
 	if err != nil {
 		log.Fatal("Error parsing YAML file")
 	}
 
+	log.Println("Converting to appropriate YAML format")
+
 	jsonEnvVars := cmapparser.ConvertToAzureJSON(envVars)
 	jsonBytes, err := json.Marshal(jsonEnvVars)
 	if err != nil {
-		log.Fatal("Eror generating JSON output file")
+		log.Fatal("Error generating JSON output file")
 	}
-	ioutil.WriteFile(*outPath, jsonBytes, 0644)
+
+	log.Println("Writing result to the temporary file...")
+	err = ioutil.WriteFile(*outPath, jsonBytes, 0644)
+	if err != nil {
+		log.Println("Succeeded! :)")
+	} else {
+		log.Fatal("Error writing file to disk!!")
+	}
 }
